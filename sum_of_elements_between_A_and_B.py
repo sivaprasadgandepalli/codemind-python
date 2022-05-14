18a35865a4d33eb2367b90fc2e9ce5a85a27fb7f
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-05-14 22:30:55 +0530

    n=int(input()) d=list(map(int,input().split())) s=0 h=0 a,b=map(int,input().split()) for i in range(0,len(d)):     if a<=d[i]<=b:         s+=d[i]
            h=1
    if h==1:
        print(s)
    else:
        print(-1)

---
+++ sum_of_elements_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+d=list(map(int,input().split()))
+s=0
+h=0
+a,b=map(int,input().split())
+for i in range(0,len(d)):
+    if a<=d[i]<=b:
+        s+=d[i]
+        
+        h=1
+if h==1:
+    print(s)
+else:
+    print(-1)
