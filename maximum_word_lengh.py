867abe4a290d962b8560d918330d9c677f5c4ef1
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-05-18 20:04:02 +0530

    def count(n):     c=0     n=list(n)     for i in n:         if i==' ':             c=c         else:             c+=1     return c n=input() d=n.split() s=[] for i in d:     k=count(i)     if k!=0:         s.append(k) print(max(s))

---
+++ maximum_word_lengh.py
@@ -0,0 +1,19 @@
+def count(n):
+    c=0
+    n=list(n)
+    for i in n:
+        if i==' ':
+            c=c
+        else:
+            c+=1
+    return c
+n=input()
+d=n.split()
+s=[]
+for i in d:
+    k=count(i)
+    if k!=0:
+        s.append(k)
+print(max(s))
+        
+        
