a97393db295341397171df094061c30a40081fe0
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2022-05-22 16:04:19 +0530

    n=int(input()) d=list(map(int,input().split())) s=[] h=0 for i in d:     c=d.count(i)     if c==i:         h=1         if i not in s:             s.append(i) if h==1:     print(*s) else:     print(-1)

---
+++ Elements_same_as_count.py
@@ -0,0 +1,15 @@
+n=int(input())
+d=list(map(int,input().split()))
+s=[]
+h=0
+for i in d:
+    c=d.count(i)
+    if c==i:
+        h=1
+        if i not in s:
+            s.append(i)
+if h==1:
+    print(*s)
+else:
+    print(-1)
+    
